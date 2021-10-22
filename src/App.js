840d50a3a4ad6b54286f8ccc636559b61e296eb2
Marcos Fierros <is702908@iteso.mx>
2021-10-22 14:14:25 -0500

    Added React Router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './components/Home/Home';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/" exact component={Home} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
