212acc0434debcb9f6b5fcf3bcbddc79aea01f2b
Marcos Fierros <is702908@iteso.mx>
2021-10-23 22:19:34 -0500

    Extra Views

---
+++ src/components/Home/MyAccount/MyAccount.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Center } from "@chakra-ui/react";
+
+function MyAccount() {
+
+  return (
+    <Center w="full" h="full">
+      MyAccount
+    </Center>
+  );
+}
+
+export default MyAccount;
